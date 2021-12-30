b736cc4de9cf95c35432b281c9995046ad9c54ea
Kashan Ahmad <toxic.headshot.2802@gmail.com>
2021-12-31 00:54:31 +0500

    :hammer: Nailed some authorized routes to enable secure routing

--- src/modules/AuthorizedRoute.js
+++ src/modules/AuthorizedRoute.js
@@ -5,15 +5,13 @@ import { supabase } from "../config/Database";
 
 /** Only authorized users can access the child of this route. */
 function AuthorizedRoute() {
-  /* // If the user is logged in.
-  return isObjectValid(supabase.auth.user()) ? (
-    <Outlet />
-  ) : (
-    // Otherwise.
-    <Navigate to="/auth/login" />
-  ); */
+  // If the user is logged in.
+  if (isObjectValid(supabase.auth.user())) {
+    return <Outlet />;
+  }
 
-  return <Outlet />;
+  // Otherwise.
+  return <Navigate to="/auth" />;
 }
 
 export { AuthorizedRoute };
