b4113a717f63b56196989b49e199964eb31f5c7e
Kashan Ahmad <toxic.headshot.2802@gmail.com>
2022-01-08 08:25:25 +0500

    :bug: fix: introduced typescript helpers

---
+++ src/helpers/strings.ts
@@ -0,0 +1,8 @@
+export const strings = {
+  // Toasts.
+  /** The toast will be removed when a newer one is displayed */
+  REMOVE_ON_PUSH: "REMOVE_ON_PUSH",
+
+  // Layout.
+  SIZES: ["smallest", "smaller", "default", "medium", "larger", "largest"],
+};
