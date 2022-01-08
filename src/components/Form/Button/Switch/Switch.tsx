a8acc163041c7a8baae0de034f171fc90c77617e
Kashan Ahmad <toxic.headshot.2802@gmail.com>
2022-01-08 08:18:40 +0500

    :bug: fix: converted Switch.js to Switch.tsx

---
+++ src/components/Form/Button/Switch/Switch.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import "./Switch.scss";
+
+interface SwitchProps extends React.ComponentPropsWithoutRef<"span"> {
+  id?: string;
+  label: string;
+  checked: boolean;
+  checkHandler: (checked: boolean) => void;
+}
+
+const Switch: React.FC<SwitchProps> = ({
+  id,
+  label,
+  checked,
+  checkHandler,
+  className,
+  ...rest
+}) => {
+  const switchRef = React.createRef<HTMLInputElement>();
+
+  return (
+    <span className={`switch ${className}`} {...rest}>
+      <label htmlFor={id} className="preTitle">
+        {label}
+      </label>
+      <button
+        className={`slider ${checked ? "checked" : ""}`}
+        onClick={() => switchRef.current?.click()}
+      >
+        <span className="thumb" />
+      </button>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(event) => checkHandler(event.target.checked)}
+        ref={switchRef}
+        hidden={true}
+      />
+    </span>
+  );
+};
+
+export { Switch };
