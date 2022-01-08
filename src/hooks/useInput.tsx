de429214ca480114e804a20e8a50f4f01acbbe3e
Kashan Ahmad <toxic.headshot.2802@gmail.com>
2022-01-08 08:26:42 +0500

    :bug: fix: converted useInput.js to useInput.tsx

---
+++ src/hooks/useInput.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { InputActions, InputState } from "../helpers/types";
+
+/** Reducer method for an input component. */
+function inputReducer(state: InputState, action: InputActions): InputState {
+  switch (action.type) {
+    case "default":
+      return { ...state, variant: "default" } as InputState;
+    case "update":
+      return { ...state, value: action.value } as InputState;
+    case "valid":
+      return {
+        ...state,
+        variant: "valid",
+        tooltip: action.tooltip,
+      } as InputState;
+    case "invalid":
+      return {
+        ...state,
+        variant: "invalid",
+        tooltip: action.tooltip,
+      } as InputState;
+    default:
+      throw new Error("No suitable action found.");
+  }
+}
+
+/** The reducer hook for an input component.
+ * Allowed actions:-
+ * - `Valid` - Makes the input have a green outline.
+ * - `Invalid` - Makes the input have a reddish outline.
+ * - `Default` - Resets all the visual changes of the input.
+ * - `Update` - Updates the value of the input.
+ *
+ * Usage:-
+ * - Valid - `{type: 'valid', tooltip: action.tooltip (scroll down)}`
+ * - Invalid - `{type: 'invalid', tooltip: action.tooltip (scroll down)}`
+ * - Default - `{type: 'default'}`
+ * - Update - `{type: 'update', value: action.value}`
+ *
+ * How to show a tooltip?
+ * Provide a tooltip object in the action. The object can have 3 properties:
+ * - `label` - The tooltip text.
+ * - `isShownForever` - Whether the tooltip is shown forever or shown on focus.
+ * - `position` - Where should the tooltip be shown. Provide one of [`top-left`, `top-right`, `bottom-right`, `bottom-left`], defaults to top-left.
+ */
+function useInput() {
+  /** The initial state of an input component. */
+  const initialState: InputState = {
+    value: "",
+    variant: "default",
+  };
+
+  return React.useReducer(inputReducer, initialState);
+}
+
+export { useInput };
