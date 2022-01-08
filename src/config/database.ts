3c89d0165deddf05db3f02942ac6a62c66027432
Kashan Ahmad <toxic.headshot.2802@gmail.com>
2022-01-08 08:24:27 +0500

    :bug: fix: introduced typescript config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { databaseUrl, databasePublicKey } from "../helpers/process";
 
+if (databaseUrl === undefined || databasePublicKey === undefined) {
+  throw new Error("Database initialization error.");
+}
+
 export const supabase = createClient(databaseUrl, databasePublicKey);
