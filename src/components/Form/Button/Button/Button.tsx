58a47ec5faa67e962308120faaed50d6760ed900
Kashan Ahmad <toxic.headshot.2802@gmail.com>
2022-01-08 08:15:17 +0500

    :bug: fix: converted Button.js to Button.tsx

---
+++ src/components/Form/Button/Button/Button.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import "./Button.scss";
+
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
+  variant?: "primary" | "secondary" | "tertiary";
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  className,
+  variant = "primary",
+  ...rest
+}) => {
+  return (
+    <button className={`button-${variant} ${className}`} {...rest}>
+      <span className="button-text">{children}</span>
+    </button>
+  );
+};
+
+export { Button };
