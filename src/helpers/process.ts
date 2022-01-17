eed7664170e1204c476bf0033395d3e6f7eff97f
Kashan Ahmad <toxic.headshot.2802@gmail.com>
2022-01-18 02:20:50 +0500

    Fixed

--- src/helpers/process.ts
+++ src/helpers/process.ts
@@ -1,7 +1,9 @@
 // ENV variables.
-const versionName = process.env.REACT_APP_VERSION_NAME;
-const authorEmail = process.env.REACT_APP_EMAIL;
-const databaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const databasePublicKey = process.env.REACT_APP_SUPABASE_ANON;
+
+const versionName = "Business Door";
+const authorEmail = "devshot.coffee@gmail.com";
+const databaseUrl = "https://sllujlbxvmomdoxppwdd.supabase.co";
+const databasePublicKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODY5ODg4NSwiZXhwIjoxOTU0Mjc0ODg1fQ.SyQonlBSaQlVRy3L7K9n03BQlMXE8l1cuLVhn5IOFUU";
 
 export { versionName, authorEmail, databaseUrl, databasePublicKey };
