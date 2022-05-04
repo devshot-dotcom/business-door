05839aff9ed0d7a86dcc0ea7c144c34a9d23f178
Kashan Ahmad <toxic.headshot.2802@gmail.com>
2022-05-05 00:48:48 +0500

    🚀 Deploy: close-modal@1.0.1 deployed
    Minor style-related updates.

--- src/components/close-button/close-button.tsx
+++ src/components/close-button/close-button.tsx
@@ -6,20 +6,16 @@ import styles from "./close-button.module.scss";
  * A robust close button.
  * @param {ComponentPropsWithoutRef<'button'>} {Object} Properties of an `HTMLButtonElement`
  * @returns {JSX.Element}
- * @version 1.0.0
+ * @version 1.0.1
  */
 function CloseButton({
   className = "",
   ...rest
 }: ComponentPropsWithoutRef<"button">): JSX.Element {
   return (
-    <Button
-      {...rest}
-      variant="primary"
-      className={`${styles.CloseButton} ${className}`}
-    >
+    <button {...rest} className={`${styles.CloseButton} ${className}`}>
       X
-    </Button>
+    </button>
   );
 }
 
